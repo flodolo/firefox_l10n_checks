9f55127f5812f73c0ed2437046936dcd5997071b
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2017-12-06 08:08:14 +0100

    Improve script output

--- qualitychecks.py
+++ qualitychecks.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+from collections import OrderedDict
 import json
 import os
 import re
@@ -71,18 +72,31 @@ class QualityCheck():
         self.domain = 'https://transvision.flod.org'
         self.api_url = '{}/api/v1'.format(self.domain)
 
+        # Get the list of supported locales
+        self.getLocales()
+
         # Store the number of plural forms for each locale
         self.plural_forms = {}
         self.getPluralForms()
+
+        # Initialize error messages
+        self.error_messages = OrderedDict()
+        for locale in self.locales:
+            self.error_messages[locale] = []
+        self.general_errors = []
 
         # Run checks
         self.checkAPI()
         if requested_check == 'all':
             self.checkViews()
 
+        # Print errors
+        self.printErrors()
+
     def getPluralForms(self):
         ''' Get the number of plural forms for each locale '''
         url = '{}/entity/gecko_strings/?id=toolkit/chrome/global/intl.properties:pluralRule'.format(self.api_url)
+        print('Reading the list of plural forms')
         try:
             response = urllib2.urlopen(url)
             locales_plural_rules = json.load(response)
@@ -92,21 +106,66 @@ class QualityCheck():
         for locale, rule_number in locales_plural_rules.iteritems():
             self.plural_forms[locale] = self.plural_rules[int(rule_number)]
 
-    def checkAPI(self):
+    def getLocales(self):
+        ''' Get the list of supported locales '''
+        print('Reading the list of supported locales')
+        url_locales = '{}/locales/gecko_strings/'.format(self.api_url)
+        try:
+            response = urllib2.urlopen(url_locales)
+            self.locales = json.load(response)
+        except Exception as e:
+            print(e)
 
+    def printErrors(self):
+        ''' Print error messages '''
+        error_count = 0
+        for locale, errors in self.error_messages.iteritems():
+            if errors:
+                print('\n----\nLocale: {} ({})'.format(locale, len(errors)))
+                error_count += len(errors)
+                for e in errors:
+                    print(u'- {}'.format(e))
+        if error_count:
+            print('\n----\nTotal errors: {}'.format(error_count))
+        if self.general_errors:
+            print('\n----\nGeneral errors ({})'.format(len(self.general_errors)))
+            self.general_errors.sort()
+            print(u'\n'.join(self.general_errors))
+
+    def sanityCheckJSON(self):
+        ''' Do a sanity check on JSON files, checking for duplicates '''
+        for json_file in self.json_files:
+            try:
+                checks = json.load(open(os.path.join(self.script_folder, 'checks', json_file + '.json')))
+            except Exception as e:
+                print('Error loading JSON file {}'.format(json_file))
+                print(e)
+                sys.exit(1)
+
+            available_checks = []
+            for c in checks:
+                id = '{}-{}-{}'.format(c['file'], c['entity'], c['type'])
+                if id in available_checks:
+                    print('ERROR: check {} is duplidated'.format(id))
+                    continue
+                available_checks.append(id)
+
+    def checkAPI(self):
+        ''' Check strings via API requests '''
+        self.sanityCheckJSON()
         if self.requested_check != 'all':
             if self.requested_check not in self.json_files:
-                print('Requested check ({}) does not exist. Available checks:'.format(self.requested_check))
-                print('\n'.join(self.json_files))
+                print('ERROR: The requested check ({}) does not exist. Available checks:'.format(self.requested_check))
+                for f in self.json_files:
+                    print('- {}'.format(f))
                 sys.exit(1)
             else:
                 self.json_files = [self.requested_check]
 
         url = '{}/entity/gecko_strings/?id={}:{}'
 
-        error_messages = []
         for json_file in self.json_files:
-            print('\n---\nCheck name: {}\n'.format(json_file))
+            print('CHECK: {}'.format(json_file))
             try:
                 checks = json.load(open(os.path.join(self.script_folder, 'checks', json_file + '.json')))
             except Exception as e:
@@ -116,12 +175,12 @@ class QualityCheck():
 
             for c in checks:
                 try:
-                    print('Checking {}'.format(c['entity']))
+                    # print('Checking {}'.format(c['entity']))
                     try:
                         response = urllib2.urlopen(url.format(self.api_url, c['file'], c['entity']))
                         json_data = json.load(response)
                     except Exception as e:
-                        error_messages.append('  ERROR checking {}:{}: {}'.format(c['file'], c['entity'], e))
+                        self.general_errors.append('Error checking {}:{}: {}'.format(c['file'], c['entity'], e))
                     for locale, translation in json_data.iteritems():
                         # Ignore some locales if exclusions are defined
                         if 'excluded_locales' in c and locale in c['excluded_locales']:
@@ -133,68 +192,44 @@ class QualityCheck():
                             for t in c['checks']:
                                 pattern = re.compile(t, re.UNICODE)
                                 if not pattern.search(translation):
-                                    error_msg = u'{}: missing {} in {}'.format(locale, t, c['entity'])
-                                    print(u'  {}'.format(error_msg))
-                                    error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                    error_msg = u'Missing {} in {}'.format(t, c['entity'])
+                                    self.error_messages[locale].append(u'{} - {}'.format(json_file, error_msg))
                         elif c['type'] == 'include':
                             for t in c['checks']:
                                 if t not in translation:
-                                    error_msg = u'{}: missing {} in {}'.format(locale, t, c['entity'])
-                                    print(u'  {}'.format(error_msg))
-                                    error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                    error_msg = u'Missing {} in {}'.format(t, c['entity'])
+                                    self.error_messages[locale].append(u'{} - {}'.format(json_file, error_msg))
                         elif c['type'] == 'equal_to':
                             if c['value'].lower() != translation.lower():
-                                error_msg = u'  {}: {} not equal to {} in {}'.format(locale, translation, c['value'], c['entity'])
-                                print(u'  {}'.format(error_msg))
-                                error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                error_msg = u'{} not equal to {} in {}'.format(translation, c['value'], c['entity'])
+                                self.error_messages[locale].append(u'{} - {}'.format(json_file, error_msg))
                         elif c['type'] == 'not_equal_to':
                             if c['value'] == translation:
-                                error_msg = u'  {}: {} is equal to {} in {}'.format(locale, translation, c['value'], c['entity'])
-                                print(u'  {}'.format(error_msg))
-                                error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                error_msg = u'{} is equal to {} in {}'.format(translation, c['value'], c['entity'])
+                                self.error_messages[locale].append(u'{} - {}'.format(json_file, error_msg))
                         elif c['type'] == 'acceptable_values':
                             if translation not in c['values']:
-                                error_msg = u'  {}: {} is not an acceptable value'.format(locale, translation)
-                                print(u'  {}'.format(error_msg))
-                                error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                error_msg = u'{}: {} is not an acceptable value'.format(translation)
+                                self.error_messages[locale].append(u'{} - {}'.format(json_file, error_msg))
                         elif c['type'] == 'typeof':
                             if type(translation) != c['value']:
-                                error_msg = u'  {}: {} is not of type'.format(locale, translation)
-                                print(u'  {}'.format(error_msg))
-                                error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                error_msg = u'{} is not of type'.format(translation)
+                                self.error_messages[locale](u'{} - {}'.format(json_file, error_msg))
                         elif c['type'] == 'bytes_length':
                             current_length = len(translation.encode('utf-8'))
                             if current_length > c['value']:
-                                error_msg = u'  {}: {} ({}) is longer than {} bytes (current length: {} bytes)'.format(locale, c['entity'], translation, c['value'], current_length)
-                                print(u'  {}'.format(error_msg))
-                                error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                error_msg = u'{} ({}) is longer than {} bytes (current length: {} bytes)'.format(c['entity'], translation, c['value'], current_length)
+                                self.error_messages[locale].append(u'{} - {}'.format(json_file, error_msg))
                         elif c['type'] == 'plural_forms':
-                            if 'excluded_locales' in c and locale in c['excluded_locales']:
-                                continue
                             num_forms = len(translation.split(';'))
                             if num_forms != self.plural_forms[locale]:
-                                error_msg = u'  {}: {} has {} plural forms (requested: {})'.format(locale, c['entity'], num_forms, self.plural_forms[locale])
-                                print(u'  {}'.format(error_msg))
-                                error_messages.append(u'  {} - {}'.format(json_file, error_msg))
+                                error_msg = u'{} has {} plural forms (requested: {})'.format(c['entity'], num_forms, self.plural_forms[locale])
+                                self.error_messages[locale].append(u'{} - {}'.format(json_file, error_msg))
                 except Exception as e:
                     print(e)
 
-        if error_messages:
-            print('\nThere are errors ({})'.format(len(error_messages)))
-            error_messages.sort()
-            print(u'\n'.join(error_messages))
-        else:
-            print('\nThere are no errors.')
-
     def checkViews(self):
         ''' Check views for access keys and keyboard shortcuts '''
-        url_locales = '{}/locales/gecko_strings/'.format(self.api_url)
-        try:
-            response = urllib2.urlopen(url_locales)
-            locales = json.load(response)
-        except Exception as e:
-            print(e)
-
         excluded_products = (
             'calendar/',
             'chat/',
@@ -204,12 +239,12 @@ class QualityCheck():
             'suite/',
         )
 
-        print('\n-----\nChecking variables\n-----')
+        print('CHECK: variables')
         f = open(os.path.join(self.script_folder, 'exceptions', 'variables.txt'), 'r')
         exceptions = []
         for l in f:
             exceptions.append(l.rstrip())
-        for locale in locales:
+        for locale in self.locales:
             url = '{}/variables/?locale={}&repo=gecko_strings&json'.format(self.domain, locale)
             response = urllib2.urlopen(url)
             variable_errors = json.load(response)
@@ -219,15 +254,15 @@ class QualityCheck():
                 error_msg = '{}: {}'.format(locale, error)
                 if error_msg in exceptions:
                     continue
-                print(error_msg)
-
+                error_msg = error_msg.replace(locale, 'variables')
+                self.error_messages[locale].append(error_msg)
 
         f = open(os.path.join(self.script_folder, 'exceptions', 'shortcuts.txt'), 'r')
         exceptions = []
         for l in f:
             exceptions.append(l.rstrip())
-        print('\n-----\nChecking keyboard shortcuts\n-----')
-        for locale in locales:
+        print('CHECK: keyboard shortcuts')
+        for locale in self.locales:
             url = '{}/commandkeys/?locale={}&repo=gecko_strings&json'.format(self.domain, locale)
             response = urllib2.urlopen(url)
             variable_errors = json.load(response)
@@ -237,7 +272,8 @@ class QualityCheck():
                 error_msg = '{}: {}'.format(locale, error)
                 if error_msg in exceptions:
                     continue
-                print(error_msg)
+                error_msg = error_msg.replace(locale, 'keyboard shortcuts')
+                self.error_messages[locale].append(error_msg)
 
 def main():
     # Parse command line options
