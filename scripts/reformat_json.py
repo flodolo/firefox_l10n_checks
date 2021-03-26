aee4dd4446b8ed5dae318c791436436e66c2e441
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2021-03-26 08:46:25 +0100

    Reformat JSON

--- scripts/reformat_json.py
+++ scripts/reformat_json.py
@@ -4,6 +4,20 @@ import glob
 import json
 import os
 
+def reorder_node(node):
+    """Reorder arrays in nodes recursively"""
+
+    if isinstance(node, list):
+        if len([x for x in node if not isinstance(x, str)]) == 0:
+            # Only strings in the list, reorder it
+            node = node.sort()
+        else:
+            for sub_node in node:
+                reorder_node(sub_node)
+
+    if isinstance(node, dict):
+        for key, sub_node in node.items():
+            reorder_node(node[key])
 
 def main():
     root_path = os.path.abspath(
@@ -14,6 +28,9 @@ def main():
     for filename in file_list:
         with open(filename, "r") as f:
             json_data = json.load(f)
+
+        reorder_node(json_data)
+
         with open(filename, "w") as f:
             json.dump(json_data, f, indent=2, sort_keys=True)
 
